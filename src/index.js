50376df0de376f3da47a2d40d7f1bb3d39ccacac
Mikhail <mikhail.epatko@gmail.com>
2019-07-16 10:48:20 +0300

    Added bindActionCreators.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
-import {createStore} from 'redux';
+import {createStore, bindActionCreators} from 'redux';
 
-import {inc, dec, rnd} from './actions';
+// import {inc, dec, rnd} from './actions';
+import * as actions from './actions';
 import reducer from './reducer';
 
 
 const store = createStore(reducer);
 
+const {dispatch, subscribe} = store;
 
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// };
+//
+// const incDispatch = bindActionCreator(inc, dispatch);
+// const decDispatch = bindActionCreator(dec, dispatch);
+// const rndDispatch = bindActionCreator(rnd, dispatch);
+//
+// const incDispatch = bindActionCreators(inc, dispatch);
+// const decDispatch = bindActionCreators(dec, dispatch);
+// const rndDispatch = bindActionCreators(rnd, dispatch);
+//
+// const {incDispatch, decDispatch, rndDispatch} = bindActionCreators({
+//   incDispatch: inc,
+//   decDispatch: dec,
+//   rndDispatch: rnd
+// }, dispatch);
+
+
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
 document
   .getElementById('inc')
-  .addEventListener('click', () => {
-    store.dispatch(inc());
-  });
+  .addEventListener('click', inc);
 
 document
   .getElementById('dec')
-  .addEventListener('click', () => {
-    store.dispatch(dec());
-  });
+  .addEventListener('click', dec);
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random() * 10);
-  store.dispatch(rnd(payload));
-});
+    rnd(payload);
+  });
 
 const update = () => {
   document
@@ -33,4 +51,4 @@ const update = () => {
     .innerText = store.getState();
 };
 
-store.subscribe(update);
+subscribe(update);
